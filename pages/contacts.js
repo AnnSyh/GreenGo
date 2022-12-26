bbff3fbccc32898f4fd5f387bacebf5d5ee6caf1
AnnSyh <ann.syharevskaya@gmail.com>
2022-12-26 18:04:32 +0300

    dell layout in contacts page

--- pages/contacts.js
+++ pages/contacts.js
@@ -1,6 +1,3 @@
-// import Layout from "../companents/Layout";
-import Layout from "../layouts/MainLayout";
-
 export default function Contacts() {
-  return <mainLayout title="Contacts">1111Страница about</mainLayout>;
+  return <main title="Contacts">1111Страница about</main>;
 }
